c8fff36fb9bc231dca1401955b8449dbee1532c3
Rafael Françozo <rvfrancozo@gmail.com>
2022-06-15 09:29:02 -0400

    -

---
+++ Assets/Scripts/Velocidade.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Velocidade : MonoBehaviour
+{
+    public Vector2 velocidade;
+
+    void FixedUpdate() {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = velocidade;
+    }
+}
+
+
